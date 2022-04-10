d4fc7057fb99a06ec5f7e840ed8430c6d0a6e3d4
pWive <moobotw@gmail.com>
2022-04-10 13:22:56 +0300

    Added message

---
+++ script.js
@@ -0,0 +1,5 @@
+const mornings = ['do some exercises', 'have breakfast', 'read the news'];
+const daytimes = ["get one hour's sleep", 'do the cleaning', 'arrange a walk for yourself', 'find an interesting book'];
+const evenings = ['watch the movie', 'play board games', 'explore the starry sky'];
+
+
