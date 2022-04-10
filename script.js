d6d8961fa71f00b5a45237454e5dc09681aa77a9
pWive <moobotw@gmail.com>
2022-04-10 13:38:28 +0300

    Added functionality for random selection

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const mornings = ['do some exercises', 'have breakfast', 'read the news'];
+const mornings = ['do some exercises', 'have breakfast', 'read the news', 'morning run'];
 const daytimes = ["get one hour's sleep", 'do the cleaning', 'arrange a walk for yourself', 'find an interesting book'];
 const evenings = ['watch the movie', 'play board games', 'explore the starry sky'];
 
+const randomNumber = arr => Math.floor(Math.random() * (arr.length));
 
+const randomChoice = (morning, daytime, evening) => {
+    return `Morning: ${mornings[morning]} \nDaytime: ${daytimes[daytime]} \nEvenings: ${evenings[evening]}`;
+}
+console.log(randomChoice(randomNumber(mornings),randomNumber(daytimes),randomNumber(evenings)))
